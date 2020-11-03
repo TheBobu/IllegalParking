3b79f4e4dabb5078a1783183c8a5cfc9d75a34ad
Florescu Andrei <andrei.florescu.00@gmail.com>
2020-11-03 21:57:26 +0200

    commit

---
+++ IllegalParking.Web/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using IllegalParking.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace IllegalParking.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public CameraViewModel cameraViewModel { get; set; }
+        bool x = false;
+        public IActionResult Index()
+        {
+            if(x)
+            {
+                return RedirectToAction("Save");
+            }
+            cameraViewModel = new CameraViewModel();
+            cameraViewModel.Picture = "a.bmp";
+            return View();
+        }
+
+        public IActionResult Save()
+        {
+            x = true;
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ IllegalParking.Web/Models/CameraViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IllegalParking.Web.Models
+{
+    public class CameraViewModel
+    {
+        public string Picture { get; set; }
+    }
+}
